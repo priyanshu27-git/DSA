13bc948f23d550892ac49a671a381c275358e2a1
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-06-09 09:41:48 +0530

    LCS 1

---
+++ Array/Medium/Longest_Consecutive_Sequence.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a, a + n);
+    int count = 1;
+    int match = 0, Longest = 1;
+    if (n == 0)
+    {
+        cout << 0;
+        return 0;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] - 1 == a[match])
+        {
+            count++;
+            match = i;
+            if (count > Longest)
+            {
+                Longest = count;
+            }
+        }
+        else if (a[i] > a[match] + 1)
+        {
+            match = i;
+            count = 1;
+        }
+    }
+
+    cout << Longest;
+
+    return 0;
+}
