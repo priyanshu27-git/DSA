e7fe26a1f5e7af8a25c7edb2b5630a1c168a8bf9
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-06-25 13:25:44 +0530

    updated code

--- Array/Hard/Merge_Intervals.cpp
+++ Array/Hard/Merge_Intervals.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     int n; 
     cin >> n;
-    vector<vector<int>> a(n , vector<int> (2));
+    vector<vector<int>> a(n ,vector<int> (2));
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < 2; j++)
