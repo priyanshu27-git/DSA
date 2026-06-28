8d9f12635121e6117006652d34fc9c655f9e9160
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-06-28 23:57:15 +0530

    Max Product of subarray

---
+++ Array/Hard/Maximum_Product_Of_Subarray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    
+    int start = 1;
+    int end = 1;
+
+    int Maximum = a[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if(start == 0) start = 1;
+
+        if(end == 0) end = 1;
+
+        start *= a[i];
+
+        end *= a[n - i - 1];
+
+        Maximum = max(Maximum , max(start, end));
+    }
+    
+    cout << Maximum;
+    return 0;
+}
