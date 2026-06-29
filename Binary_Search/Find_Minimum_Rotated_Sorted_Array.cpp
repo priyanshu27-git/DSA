eb827764e83c704ce653a6be9c70ac030a626396
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-06-29 23:42:37 +0530

    find min

---
+++ Binary_Search/Find_Minimum_Rotated_Sorted_Array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int Min(int a[], int low, int high){
+
+    while (low < high)
+    {
+        int mid = (low + high) / 2;
+
+        if(a[mid] > a[high]){
+            low = mid + 1;
+        }
+
+        else{
+            high = mid;
+        }
+    }
+    return a[low];
+}    
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int ans = Min(a , 0 , n - 1);
+
+    cout << ans;
+
+    return 0;
+}
