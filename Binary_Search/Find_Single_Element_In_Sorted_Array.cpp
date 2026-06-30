9c05e0362c5375e2ab389967fec6b192c6e0ad24
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-06-30 23:16:54 +0530

    Find single element in array

---
+++ Binary_Search/Find_Single_Element_In_Sorted_Array.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    if (n == 1)
+    {
+        cout << a[0];
+        return a[0];
+    }
+
+    if (a[0] != a[1])
+    {
+        cout << a[0];
+        return 0;
+    }
+    if (a[n - 1] != a[n - 2])
+    {
+        cout << a[n - 1];
+        return 0;
+    }
+
+    int low = 1, high = n - 2;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (a[mid] != a[mid + 1] && a[mid] != a[mid - 1])
+        {
+            cout << a[mid];
+            break;
+        }
+
+        if (mid % 2 == 1 && a[mid] == a[mid - 1] || mid % 2 == 0 && a[mid] == a[mid + 1])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return 0;
+}
