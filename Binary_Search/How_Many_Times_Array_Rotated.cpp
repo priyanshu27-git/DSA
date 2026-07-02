32d44ac99f76d3e441fcd27d43e8635256b34d06
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-02 23:49:44 +0530

    count Array rotated

---
+++ Binary_Search/How_Many_Times_Array_Rotated.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int Rotated(int a[], int low, int high){
+
+    while (low < high)
+    {
+        int mid = (low + high) / 2;
+
+        if(a[mid] > a[high]){
+            low = mid + 1;
+        }
+
+        else{
+            high = mid;
+        }
+    }
+    return low;
+}    
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int ans = Rotated(a , 0 , n - 1);
+
+    cout << ans;
+
+    return 0;
+}
