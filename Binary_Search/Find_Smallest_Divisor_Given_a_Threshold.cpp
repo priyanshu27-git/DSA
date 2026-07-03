751ac7a9d93a642e979b26db53021fb45a57b28a
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-03 23:30:34 +0530

    Smallest diviosr done

---
+++ Binary_Search/Find_Smallest_Divisor_Given_a_Threshold.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int smallest_divisor(vector<int> a, int value)
+{
+    int sum = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        sum += (a[i] + value - 1) / value;
+    }
+    return sum;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int Threshold;
+    cin >> Threshold;
+
+    int Maxi = a[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        Maxi = max(Maxi, a[i]);
+    }
+
+    int low = 1, high = Maxi;
+
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        
+
+        int divisor = smallest_divisor(a, mid);
+
+        if (divisor <= Threshold)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    cout << low;
+
+    return 0;
+}
