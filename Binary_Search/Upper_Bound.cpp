2a78afc7e56761d52da1ce2f60479f3140c4e52d
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-04 23:33:58 +0530

    Upper bound done

---
+++ Binary_Search/Upper_Bound.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int UpperBound(int a[] , int n , int Target, int low , int high){
+
+    int ans = n;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if(a[mid] > Target){
+            ans = mid;
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return ans;
+
+
+    // while (low <= high)
+    // {
+    //     int mid = (low + high) / 2;
+    //     if(a[mid] > Target){
+    //         if(mid - 1 >= 0 && a[mid - 1] > Target){
+    //             high = mid  - 1;
+    //         }
+    //         else
+    //             return mid;
+    //     }
+
+    //     if(a[mid] <= Target){
+    //         low = mid + 1;
+    //     }
+    //     else{
+    //         high = mid - 1;
+    //     }
+    // }
+    // return n;
+}
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int Target;
+    cin >> Target;
+
+    int ans = UpperBound(a, n , Target , 0 , n - 1);
+
+    cout << ans;
+    return 0;
+}
