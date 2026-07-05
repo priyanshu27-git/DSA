b20cfc989593d8129c6a1bbf62a877369737646a
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-05 20:44:59 +0530

    search in rotated sorted array 2nd version

---
+++ Binary_Search/Second_Version_Search_Rotated_Sorted_Array.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+bool Search(int a[], int low, int high, int Target){
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if(a[mid] == Target){
+            return true;
+        }
+
+        if(a[low] == a[mid] && a[mid] == a[high]){
+            low++;
+            high--;
+            continue;
+        }
+
+        if(a[low] <= a[mid]){
+            if(Target >= a[low] && Target <= a[mid]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        else{
+            if(Target >= a[mid] && Target <= a[high]){
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+    }
+    return false;
+}    
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int Target;
+    cin >> Target;
+
+    bool ans = Search(a , 0 , n - 1, Target);
+
+    if(ans) 
+        cout << "true";
+    else
+        cout << "false";
+
+
+    return 0;
+}
