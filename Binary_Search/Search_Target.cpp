f0b45c10d85fbb607a7653fead55563b08ba9254
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-05 20:48:59 +0530

    code optimized

--- Binary_Search/Search_Target.cpp
+++ Binary_Search/Search_Target.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int BinarySearch(int a[] , int Target, int low , int high){
+int BinarySearch(int a[], int Target, int low, int high)
+{
 
     while (low <= high)
     {
         int mid = (low + high) / 2;
-        if(Target == a[mid]){
+        if (Target == a[mid])
+        {
             return mid;
         }
 
-        if(a[mid] < Target){
+        if (a[mid] < Target)
+        {
             low = mid + 1;
         }
-        
-        else{
+
+        else
+        {
             high = mid - 1;
         }
     }
     return -1;
 }
-int main() {
+int main()
+{
     int n;
     cin >> n;
     int a[n];
@@ -31,7 +36,7 @@ int main() {
     int Target;
     cin >> Target;
 
-    int ans = BinarySearch(a, Target , 0 , n - 1);
+    int ans = BinarySearch(a, Target, 0, n - 1);
 
     cout << ans;
     return 0;
