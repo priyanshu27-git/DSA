7e970223d03e8b977755429e5792ae5766dca81f
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-06 10:30:00 +0530

    Update 2

--- Binary_Search/Koko_Eating_Banana.cpp
+++ Binary_Search/Koko_Eating_Banana.cpp
@@ -3,39 +3,48 @@
 #include <cmath>
 using namespace std;
 
- int findMax(vector<int>& a){
-        int Max = a[0];
-        for(int i = 0; i < a.size(); i++){
-            Max = max(Max , a[i]);
-        }    
-        return Max;
+int findMax(vector<int> &a)
+{
+    int Max = a[0];
+    for (int i = 0; i < a.size(); i++)
+    {
+        Max = max(Max, a[i]);
     }
+    return Max;
+}
 
-    long long Calculate_Hours(vector<int>& a, int speed){
-        long long TotalH = 0;
-        for(int i = 0; i < a.size(); i++){
-            TotalH += (long long)ceil((long double)a[i] / speed);
-        }
-        return TotalH;
+long long Calculate_Hours(vector<int> &a, int speed)
+{
+    long long TotalH = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        TotalH += (long long)ceil((long double)a[i] / speed);
     }
-    int minEatingSpeed(vector<int>& a, int h) {
-       int low = 1 , high = findMax(a);
-       int ans = 0;
-        while(low <= high){
-            int mid = (low + high) / 2;
+    return TotalH;
+}
+int minEatingSpeed(vector<int> &a, int h)
+{
+    int low = 1, high = findMax(a);
+    int ans = 0;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
 
-            long long totalH  = Calculate_Hours(a , mid);
-            if(totalH <= h){
-                ans = mid;
-                high = mid - 1;
-            }
-            else{
-                low = mid + 1;
-            }
+        long long totalH = Calculate_Hours(a, mid);
+        if (totalH <= h)
+        {
+            ans = mid;
+            high = mid - 1;
         }
-        return ans;
+        else
+        {
+            low = mid + 1;
+        }
     }
-int main() {
+    return ans;
+}
+int main()
+{
     int n;
     cin >> n;
     vector<int> a(n);
@@ -46,9 +55,9 @@ int main() {
     long long h;
     cin >> h;
 
-    int ans = minEatingSpeed(a , h);
+    int ans = minEatingSpeed(a, h);
 
     cout << ans;
-    
+
     return 0;
 }
