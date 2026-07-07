ca8a4d8155756d74a6099c006d0f3f47af6d65d4
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-07 12:37:50 +0530

    Day 6

---
+++ Binary_Search/Kth_Missing_Positive_Number.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int k;
+    cin >> k;
+
+    // Binary Search approach(O(logn))
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        int missing = a[mid] - (mid + 1);
+
+        if (missing < k)
+        {
+            low = mid + 1;
+        }
+
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    cout << low + k; // (high + 1 = low)
+
+    return 0;
+}
+
+// for (int i = 0; i < n; i++)
+// {
+//     if(a[i] <= k){
+//         k++;
+//     }
+//     else {
+//          cout << k;
+//          break;
+//     }
+// }
