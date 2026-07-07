5fd4ea19123f2c8bf876f2b844cf187928d8db4a
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-07 12:44:09 +0530

    Final Commit

--- Binary_Search/Last_Occurenece_In_Array.cpp
+++ Binary_Search/Last_Occurenece_In_Array.cpp
@@ -1,44 +1,51 @@
 #include <iostream>
 using namespace std;
 
-int last(int a[], int low, int high, int Target, int n){
+int last(int a[], int low, int high, int Target, int n)
+{
 
     int ans = -1;
-	while (low <= high){
-		int mid = (low + high) / 2;
-			
-		if (a[mid] == Target) {
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (a[mid] == Target)
+        {
             ans = mid;
             low = mid + 1;
-		}
-		else if(a[mid] > Target){
+        }
+        else if (a[mid] > Target)
+        {
             high = mid - 1;
-		}
+        }
         else
             low = mid + 1;
-	}
-	return ans;
+    }
+    return ans;
 }
 
-int first(int a[], int low, int high, int Target, int n){
-    
+int first(int a[], int low, int high, int Target, int n)
+{
+
     int ans = -1;
-	while (low <= high){
-		int mid = (low + high) / 2;
-			
-		if (a[mid] == Target) {
-			ans = mid;
-			high = mid - 1;
-		}
-		else if(a[mid] > Target){
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (a[mid] == Target)
+        {
+            ans = mid;
             high = mid - 1;
-		}
+        }
+        else if (a[mid] > Target)
+        {
+            high = mid - 1;
+        }
         else
             low = mid + 1;
-	}
-	return ans;
+    }
+    return ans;
 }
-
 
 int main()
 {
@@ -47,18 +54,17 @@ int main()
     int a[n];
     for (int i = 0; i < n; i++)
     {
-        cin >> a[i];  
+        cin >> a[i];
     }
     int Target;
     cin >> Target;
 
-    int floor , ceil;
+    int floor, ceil;
 
     ceil = first(a, 0, n - 1, Target, n);
     floor = last(a, 0, n - 1, Target, n);
 
     cout << ceil << " " << floor;
-
 
     return 0;
 }
