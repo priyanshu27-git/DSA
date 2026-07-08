16de2c4d5e8ac59fe817bdf6a5c41a02c29464d3
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-08 22:51:33 +0530

    updated

--- Binary_Search/Least_capacity_to_ship_in_Days.cpp
+++ Binary_Search/Least_capacity_to_ship_in_Days.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 int Minimum_days(vector<int> a, int capacity)
 {
-    int load = 0 , days = 1;
+    int load = 0, days = 1;
     for (int i = 0; i < a.size(); i++)
     {
-        if(load + a[i] > capacity){
+        if (load + a[i] > capacity)
+        {
             days = days + 1;
             load = a[i];
         }
-        else{
+        else
+        {
             load += a[i];
         }
     }
@@ -26,12 +28,13 @@ int main()
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
-    }  
+    }
 
     int days;
     cin >> days;
 
     int Maxi = a[0];
+    
     int sum = 0;
 
     for (int i = 0; i < n; i++)
@@ -45,7 +48,7 @@ int main()
     while (low <= high)
     {
         int mid = (low + high) / 2;
-        
+
         int required_days = Minimum_days(a, mid);
 
         if (required_days <= days)
