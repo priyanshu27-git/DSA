4a7d153552680cfaa4984caf95d0149d9559ac92
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-09 23:35:09 +0530

    update 3

--- Binary_Search/Floor_And_Ceil.cpp
+++ Binary_Search/Floor_And_Ceil.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int Floor(int a[], int low, int high, int Target, int n){
+int Floor(int a[], int low, int high, int Target, int n)
+{
 
-    int ans = -1;
-	while (low <= high){
+	int ans = -1;
+	while (low <= high)
+	{
 		int mid = (low + high) / 2;
-			
-		if (a[mid] > Target) {
-			high = mid - 1;   
+
+		if (a[mid] > Target)
+		{
+			high = mid - 1;
 		}
-		else {
+		else
+		{
 			ans = mid;
 			low = mid + 1;
 		}
@@ -18,42 +22,45 @@ int Floor(int a[], int low, int high, int Target, int n){
 	return ans;
 }
 
-int Ceil(int a[], int low, int high, int Target, int n){
-    
-    int ans = -1;
-	while (low <= high){
+int Ceil(int a[], int low, int high, int Target, int n)
+{
+
+	int ans = -1;
+	while (low <= high)
+	{
 		int mid = (low + high) / 2;
-			
-		if (a[mid] >= Target) {
+
+		if (a[mid] >= Target)
+		{
 			ans = mid;
 			high = mid - 1;
 		}
-		else {
+		else
+		{
 			low = mid + 1;
 		}
 	}
 	return ans;
 }
 
-
 int main()
 {
-    int n;
-    cin >> n;
-    int a[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-    int Target;
-    cin >> Target;
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	int Target;
+	cin >> Target;
 
-    int floor , ceil;
+	int floor, ceil;
 
-    floor = Floor(a, 0, n - 1, Target, n);
-    ceil = Ceil(a, 0, n - 1, Target, n);
+	floor = Floor(a, 0, n - 1, Target, n);
+	ceil = Ceil(a, 0, n - 1, Target, n);
 
-    cout << floor << " " << ceil;
+	cout << floor << " " << ceil;
 
-    return 0;
+	return 0;
 }
