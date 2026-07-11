dfe4f9c15cf7893ac25ec29dbcbbde5062c41717
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-11 22:58:14 +0530

    code updated

--- Binary_Search/Kth_Element_In_Sorted_Array.cpp
+++ Binary_Search/Kth_Element_In_Sorted_Array.cpp
@@ -5,14 +5,13 @@ int main()
 {
     int n1, n2;
     cin >> n1 >> n2;
-    
+
     int a1[n1], a2[n2];
 
     for (int i = 0; i < n1; i++) // taking input in array 1
     {
         cin >> a1[i];
     }
-
     for (int i = 0; i < n2; i++) // taking input in array 2
     {
         cin >> a2[i];
