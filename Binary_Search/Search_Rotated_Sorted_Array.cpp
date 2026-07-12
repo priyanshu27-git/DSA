3132436866898ed2d31ddfcfedcd777fd367494e
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-12 23:00:20 +0530

    hello

--- Binary_Search/Search_Rotated_Sorted_Array.cpp
+++ Binary_Search/Search_Rotated_Sorted_Array.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int Search(int a[], int low, int high, int Target){
+int Search(int a[], int low, int high, int Target)
+{
 
     while (low <= high)
-    {   
+    {
         int mid = (low + high) / 2;
 
-        if(a[mid] == Target){
+        if (a[mid] == Target)
+        {
             return mid;
         }
 
-        if(a[low] <= a[mid]){
-            if(Target >= a[low] && Target <= a[mid]){
+        if (a[low] <= a[mid])
+        {
+            if (Target >= a[low] && Target <= a[mid])
+            {
                 high = mid - 1;
             }
-            else{
+            else
+            {
                 low = mid + 1;
             }
         }
-        else{
-            if(Target >= a[mid] && Target <= a[high]){
+        else
+        {
+            if (Target >= a[mid] && Target <= a[high])
+            {
                 low = mid + 1;
             }
-            else {
+            else
+            {
                 high = mid - 1;
             }
         }
     }
-    return - 1;
-}    
+    return -1;
+}
 
 int main()
 {
@@ -43,10 +51,9 @@ int main()
     int Target;
     cin >> Target;
 
-    int ans = Search(a , 0 , n - 1, Target);
+    int ans = Search(a, 0, n - 1, Target);
 
     cout << ans;
-
 
     return 0;
 }
