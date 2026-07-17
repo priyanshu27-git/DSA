4b80d252b8ae49ce9af271f8c40378acee109d25
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-17 23:08:31 +0530

    update 7

--- Binary_Search/Aggresive_Cows.cpp
+++ Binary_Search/Aggresive_Cows.cpp
@@ -28,13 +28,14 @@ int main()
     int n;
     cin >> n;
     int a[n];
-    
+
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
 
     int k;
+    
     cin >> k;
 
     sort(a, a + n);
