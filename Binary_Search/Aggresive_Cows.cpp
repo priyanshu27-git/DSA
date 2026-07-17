55922ea96bbbe6b2dcfcc903b3d7f07d371514c8
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-07-17 23:06:44 +0530

    update 1

---
+++ Binary_Search/Aggresive_Cows.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool max_difference(int a[], int dist, int n, int cows)
+{
+    int count_cows = 1, last = a[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] - last >= dist)
+        {
+            count_cows++;
+            last = a[i];
+        }
+
+        if (count_cows >= cows)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int k;
+    cin >> k;
+
+    sort(a , a + n);
+
+    int low = 0, high = a[n-1] - a[0];
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        bool check_max_difference = max_difference(a, mid, n , k);
+
+        if (check_max_difference)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    cout << high;
+
+    return 0;
+}
