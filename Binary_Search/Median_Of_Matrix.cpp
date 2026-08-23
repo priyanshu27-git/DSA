59b69d38300cf58460c8d6d8d781929e1ec15ec7
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-08-23 15:18:25 +0530

    code updated

--- Binary_Search/Median_Of_Matrix.cpp
+++ Binary_Search/Median_Of_Matrix.cpp
@@ -2,40 +2,45 @@
 #include <vector>
 using namespace std;
 
-int UpperBound(vector<int> &a , int n , int Target, int low , int high){
+int UpperBound(vector<int> &a, int n, int Target, int low, int high)
+{
 
     int ans = n;
 
     while (low <= high)
     {
         int mid = (low + high) / 2;
- 
-        if(a[mid] > Target){
+
+        if (a[mid] > Target)
+        {
             ans = mid;
             high = mid - 1;
-        }  
+        }
 
-        else{
+        else
+        {
             low = mid + 1;
         }
     }
     return ans;
 }
 
-int count_smaller_than_mid(vector<vector<int>> &a , int n , int m , int x){
-   
+int count_smaller_than_mid(vector<vector<int>> &a, int n, int m, int x)
+{
+
     int count = 0;
- 
-    for (int i = 0; i < n; i++)   
+
+    for (int i = 0; i < n; i++)
     {
-        count += UpperBound(a[i] , m , x , 0 , m - 1); 
+        count += UpperBound(a[i], m, x, 0, m - 1);
     }
     return count;
 }
-int main() {
-    int n , m;
+int main()
+{
+    int n, m;
     cin >> n >> m;
-    vector<vector<int>> a (n , vector<int> (m));
+    vector<vector<int>> a(n, vector<int>(m));
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -44,30 +49,30 @@ int main() {
         }
     }
 
-
-    int low = a[0][0] , high = a[0][0];
+    int low = a[0][0], high = a[0][0];
     for (int i = 0; i < n; i++)
     {
-        low = min(low , a[i][0]);
-        high = max(high , a[i][m-1]);
+        low = min(low, a[i][0]);
+        high = max(high, a[i][m - 1]);
     }
-    
 
     int req = (n * m) / 2;
     while (low <= high)
     {
         int mid = (low + high) / 2;
-        
-        int count_left_elements = count_smaller_than_mid(a , n , m , mid);
 
-        if(count_left_elements <= req){
+        int count_left_elements = count_smaller_than_mid(a, n, m, mid);
+
+        if (count_left_elements <= req)
+        {
             low = mid + 1;
         }
-        else{
+        else
+        {
             high = mid - 1;
         }
     }
-    
+
     cout << low;
 
     return 0;
