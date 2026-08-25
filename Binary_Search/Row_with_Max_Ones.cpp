18bc84f5dcc2fac9a7160bd6cd8a15b8e5fcc078
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-08-25 14:37:11 +0530

    first commit

---
+++ Binary_Search/Row_with_Max_Ones.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+// Lower bound means minimum greatest or equal to target number
+int LowerBound(int a[], int n, int Target, int low, int high)
+{
+
+    int ans = n;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (a[mid] >= Target)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+    int main()
+    {
+        int n , m;
+        cin >> n  >> m;
+        int a[n][m];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cin >> a[i][j];
+            }
+        }
+        
+        int max_count = 0;
+        int index = -1;
+        for (int i = 0; i < n; i++)
+        {
+            int count_ones = m - LowerBound(a[i] , m , 1 , 0 , n - 1);
+            if(count_ones > max_count){
+                max_count = count_ones;
+                index = i;
+            }
+        }
+
+        cout << index;
+        return 0;
+    }
