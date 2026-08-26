aadb11d4f4996be2e97523624405bc01c992fd95
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-08-26 19:54:31 +0530

    6 commit

--- Contest_Questions/Codeforces/Bigrams.cpp
+++ Contest_Questions/Codeforces/Bigrams.cpp
@@ -35,13 +35,14 @@ void solve()
     {
         cout << "NO\n";
     }
-    
+
 }
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
+    
     if (cin >> t)
     {
         while (t--)
