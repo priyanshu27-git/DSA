868e3a331aeea5bb93b8f5ee9a36a8d27fed1e1d
priyanshu27-git <sahupriyanshu888a@gmail.com>
2026-09-02 22:51:30 +0530

    Contest question done

---
+++ Contest_Questions/Codeforces/Div_2/7_july.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int x) {
+    if (x < 2) return false;
+    for (int i = 2; i * i <= x; i++) {
+        if (x % i == 0) return false;
+    }
+    return true;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    // Check if n + 1 is prime 
+    if (isPrime(n + 1)) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
